6f99a4ddb28d3fff7be69554debcc4f0addd9a51
Buriak-byte <oleksii.buriak12@gmail.com>
2026-05-01 16:00:33 +0300

    6

--- script.js
+++ script.js
@@ -15,3 +15,183 @@ visokosnyiYearInput.addEventListener("input", (e) => {
 function leapYear(year) {
   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const scientists = [
+  {
+    name: "Isaac",
+    surname: "Newton",
+    born: 1643,
+    dead: 1727,
+    id: 2,
+  },
+  {
+    name: "Galileo",
+    surname: "Galilei",
+    born: 1564,
+    dead: 1642,
+    id: 3,
+  },
+  {
+    name: "Marie",
+    surname: "Curie",
+    born: 1867,
+    dead: 1934,
+    id: 4,
+  },
+  {
+    name: "Johannes",
+    surname: "Kepler",
+    born: 1571,
+    dead: 1630,
+    id: 5,
+  },
+  {
+    name: "Nicolaus",
+    surname: "Copernicus",
+    born: 1473,
+    dead: 1543,
+    id: 6,
+  },
+  {
+    name: "Max",
+    surname: "Planck",
+    born: 1858,
+    dead: 1947,
+    id: 7,
+  },
+  {
+    name: "Albert",
+    surname: "Einstein",
+    born: 1879,
+    dead: 1955,
+    id: 1,
+  },
+  {
+    name: "Katherine",
+    surname: "Blodgett",
+    born: 1898,
+    dead: 1979,
+    id: 8,
+  },
+  {
+    name: "Ada",
+    surname: "Lovelace",
+    born: 1815,
+    dead: 1852,
+    id: 9,
+  },
+  {
+    name: "Sarah E.",
+    surname: "Goode",
+    born: 1855,
+    dead: 1905,
+    id: 10,
+  },
+  {
+    name: "Lise",
+    surname: "Meitner",
+    born: 1878,
+    dead: 1968,
+    id: 11,
+  },
+  {
+    name: "Hanna",
+    surname: "Hammarström",
+    born: 1829,
+    dead: 1909,
+    id: 12,
+  },
+];
+function bornIn19ST(scientists) {
+  const whobornIn19ST = scientists.filter((human) => {
+    return 1800 <= human.born && human.born <= 1900;
+  });
+  return whobornIn19ST;
+}
+console.log(bornIn19ST(scientists));
+function orderYears(scientists) {
+  const orderYearsArr = scientists.sort((a, b) => {
+    const lifeYearsA = a.dead - a.born;
+    const lifeYearsB = b.dead - b.born;
+    return lifeYearsA - lifeYearsB
+  });
+  return orderYearsArr;
+}
+console.log(orderYears(scientists));
+function futureborned(scientists){
+   const orderYearsArr = scientists.sort((a, b) => {
+
+     return b.born - a.born;
+   });
+   return orderYearsArr[0];
+} 
+console.log(futureborned(scientists));
+function yearsOfAlbertEnstein(scientists){
+  const AlbertEnsteinYears = scientists.find((AlbertEnstein) => {
+    return AlbertEnstein.born === 1879;})
+return AlbertEnsteinYears.born
+}
+console.log(yearsOfAlbertEnstein(scientists))
+
+
+
+
+
+function StartC(scientists) {
+  scientists.filter((scientist) => {
+   return scientist.surname.includes("C")
+  });
+  return StartC
+} 
+console.log(StartC(scientists))
